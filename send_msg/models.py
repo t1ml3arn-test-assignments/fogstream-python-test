67b241e2c2268adab6524d87f2baa0f269ec82fc
Tim <9349164+T1mL3arn@users.noreply.github.com>
2019-02-07 15:55:17 +1000

    change message model

--- send_msg/models.py
+++ send_msg/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Message(models.Model):
     text = models.TextField(max_length=1000, help_text='Write your message here')
-    receiver = models.EmailField(max_length=256)
+    receiver = models.EmailField(max_length=254)
     success = models.BooleanField(default=False)
     date = models.DateTimeField("Send date", auto_now=False, auto_now_add=False)
-    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
